f61392ab92516a0d3aadea2d6203bacc782d0df7
Arthur Vieira <Arthurv720@gmail.com>
2025-04-12 10:00:56 -0300

    Inicializando o projeto

---
+++ src/config/db.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const connectDB = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_URI as string);
+      console.log("MongoDB conectado com sucesso!");
+    } catch (error) {
+      console.error("Erro ao conectar no MongoDB:", error);
+      process.exit(1);
+    }
+  };
+
